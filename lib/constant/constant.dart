896509babb3f425b9a6a1a66b80d55718e90e8e6
appdevina <devina@completeselular.com>
2022-12-19 10:57:10 +0700

    ganti base url

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,3 +1,3 @@
-const String baseUrl = 'http://csa1backup.sytes.net:8888';
+const String baseUrl = 'http://modul.sytes.net:8888';
 const String baseUrlDocs = baseUrl + '/storage/dokumen/';
 const String baseApi = baseUrl + '/api/';
