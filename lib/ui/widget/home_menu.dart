4147edc842585b75f1ef1e5bf8ba93a200fea7ea
appdevina <devina@completeselular.com>
2023-07-06 09:42:11 +0700

    size subtitle

--- lib/ui/widget/home_menu.dart
+++ lib/ui/widget/home_menu.dart
@@ -35,7 +35,7 @@ class HomeMenu extends StatelessWidget {
               title,
               textAlign: TextAlign.left,
               style: subtTitle
-                  .copyWith(fontSize: 25, color: Colors.white, shadows: [
+                  .copyWith(fontSize: 22, color: Colors.white, shadows: [
                 const Shadow(
                   color: Colors.black,
                   blurRadius: 40,
